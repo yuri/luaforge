de8c3fe2f8fbc93480684a037bed53e3d12dbb85
Yuri Takhteyev <yuri@freewisdom.org>
2010-08-01 17:09:23 -0300

    Listing projects by the first letter.

--- luaforge/lua/sputnik/actions/luaforge.lua
+++ luaforge/lua/sputnik/actions/luaforge.lua
@@ -40,3 +40,132 @@ function actions.show_project(node, request, sputnik)
 
    return node.wrappers.default(node, request, sputnik)
 end
+
+function actions.show_project_list(node, request, sputnik)
+   local node_hash, node_ids, num_hidden = node:get_visible_children(request.user or "Anonymous")
+   local non_proto_nodes = {}
+   for i, id in ipairs(node_ids) do
+      n = node_hash[id]
+      if n.id ~= node.id .. "/@Child" then
+         table.insert(non_proto_nodes, n)
+      end
+   end
+
+   local template = node.translator.translate(node.html_content)
+
+   local nodes_by_first_letter = {}
+   local letters = {}
+   for i, node in ipairs(non_proto_nodes) do
+      local id = node.id:sub(10)
+      local prefix = ""
+      local luafreeid = id
+      if id:sub(0,4)=="lua-" then
+         prefix = id:sub(0,4)
+         luafreeid = id:sub(5)
+      elseif id:sub(0,3) == "lua" then
+         prefix = id:sub(0,3)
+         luafreeid = id:sub(4)
+      end
+      local first_letter = luafreeid:sub(1,1)
+      if first_letter:match("%d") then
+         first_letter = "0-9"
+      end
+      if not nodes_by_first_letter[first_letter] then
+         table.insert(letters, first_letter)
+         nodes_by_first_letter[first_letter] = {}
+      end
+      table.insert(nodes_by_first_letter[first_letter], {node, prefix, luafreeid})
+   end
+   table.sort(letters)
+
+   local values = {
+      new_id  = node.id .. "/new",
+      new_url = sputnik:make_url(node.id.."/new", "edit"),
+      id      = node.id,
+      content = node.content,
+      markup = function(params)
+         return node.markup.transform(params[1], node)
+      end,
+
+      do_sample_projects = function()
+
+
+         for i=1,3 do
+
+            local rand = math.ceil(math.random()*(#non_proto_nodes))
+            local node = non_proto_nodes[rand]
+            cosmo.yield{
+               short_id = node.id:sub(10),
+               abstract = node.abstract,
+               url = sputnik:make_url(node.id),
+            }
+         end
+
+      end,
+
+      do_letters = function()
+
+         for i,letter in ipairs(letters) do
+
+            local nodes = nodes_by_first_letter[letter]
+
+            table.sort(nodes, function(x,y) return x[3] < y[3] end)
+
+
+            local num_cols = 5
+            local step = math.ceil(#nodes / num_cols)
+
+            cosmo.yield {
+               letter = letter:upper(),
+               do_columns = function()
+                  for col=1,num_cols do
+                     cosmo.yield{
+                        do_nodes = function()
+                           for i=step*(col-1)+1,step*col do --node_record in ipairs(nodes) do
+                              local node_record = nodes[i]
+                              if node_record then
+                                 local node, prefix, luafreeid = unpack(node_record)
+                                  cosmo.yield{
+                                     short_id = id,
+                                     prefix = prefix,
+                                     luafreeid = luafreeid,
+                                     abstract = node.abstract,
+                                     url = sputnik:make_url(node.id)
+                                  }
+                              end
+                           end
+                        end
+                     }
+                  end
+               end  
+            }
+         end
+      end,
+   }
+
+   for k,v in pairs(node.template_helpers or {}) do
+      values[k] = v
+   end
+
+   for k,v in pairs(node.fields) do
+      if not values[k] then
+         values[k] = tostring(node[k])
+      end
+   end
+
+   
+   for action_name in pairs(node.actions) do
+      if node:check_permissions(request.user, action_name) then
+         sputnik.logger:debug("Action: " .. tostring(action_name))
+         values[action_name .. "_link"] = sputnik:make_url(node.id, action_name)
+         values["if_user_can_" .. action_name] = cosmo.c(true){}
+      else
+         values["if_user_can_" .. action_name] = cosmo.c(false){}
+      end
+   end
+
+   node.inner_html = cosmo.fill(template, values)
+   return node.wrappers.default(node, request, sputnik)
+end
+
+
--- luaforge/lua/sputnik/node_defaults/projects.lua
+++ luaforge/lua/sputnik/node_defaults/projects.lua
@@ -1,25 +1,45 @@
 module(...)
 NODE = {
-title          = "projects",
-prototype      = "@Collection"
+title          = "The Project Catalogue",
+prototype      = "@Collection",
+actions        = [[
+show = "luaforge.show_project_list"
+
+]]
 }
 NODE.html_content   = [=[$markup{$content}
 
-<br/><br/>
+<h2>Random Selection</h2>
+<dl>
+$do_sample_projects[[
+  <dd><a href="$url" style="font-size:120%;text-decoration:none" title="$abstract">$short_id</b></a></dd>
+  <dt style="margin-left:2em;margin-bottom:1em;">$abstract</dt>
+]]
+</dl>
 
-<table class="sortable" width="100%">
+<h2>All Projects</h2>
+
+<!--table class="sortable" width="100%">
  <thead>
   <tr>
    <th width="20%">name</th>
    <th>description</th>
   </tr>
- </thead>
+ </thead-->
+ $do_letters[====[
+ <h3>$letter</h3>
+
+ <table class="invisible" borders=0 width="100%"><tr>
+ $do_columns[=====[
+ <td width="20%">
+   
  $do_nodes[[
-  <tr>
-   <td><a href="$url" style="font-size:120%;text-decoration:none">$title</a></td>
-   <td>$abstract</td>
-  </tr>
+  <a href="$url" style="font-size:150%; text-decoration:none" title="$abstract">$prefix<b>$luafreeid</b></a><br/>
  ]]
+ </td>
+ ]=====]
+ </tr>
  </table>
+ ]====]
 ]=]
-NODE.content        = "Projects blah blah blah blah email blah blah blah."
+NODE.content        = "  "
