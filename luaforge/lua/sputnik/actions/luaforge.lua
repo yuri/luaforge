1df4f2938f6096879df761f96d203c2f91a7a2a6
Yuri Takhteyev <yuri@freewisdom.org>
2010-07-31 22:50:20 -0300

    A few changes form May.

--- luaforge/lua/sputnik/actions/luaforge.lua
+++ luaforge/lua/sputnik/actions/luaforge.lua
@@ -5,12 +5,22 @@ local wiki = require("sputnik.actions.wiki")
 local cosmo = require("cosmo")
 local util = require("sputnik.util")
 
+DEFAULT_ICON = "http://sputnik.freewisdom.org/en/icons/user.png"
 
 function actions.show_project(node, request, sputnik)
    
    local split = function(value, sep)
-      sep = sep or ","
+      sep = sep or ", *"
       return ipairs({util.split(value or "", sep)})
+   end
+
+   local auth = sputnik.auth
+   local function get_icon(id)
+      if auth:user_exists(id) then
+         local icon = auth:get_metadata(id, "twitter_icon")
+         if icon then return icon end
+      end
+      return DEFAULT_ICON
    end
 
    local lists = node.markup.transform(node.lists or "")
@@ -18,9 +28,10 @@ function actions.show_project(node, request, sputnik)
    node.inner_html = cosmo.f(node.html_content){
                                abstract = node.abstract,
                                website = node.website,
-                               lists = lists,
-                               releases = releases,
-                               do_authors = function() for i,v in split(node.creator) do cosmo.yield{id=v} end end,
+                               if_lists = cosmo.c(string.len(node.lists or "") > 0){lists=lists},
+                               if_releases = cosmo.c(string.len(node.releases or "") > 0){releases = releases},
+                               do_authors = function() for i,v in split(node.creator) do cosmo.yield{id=v, icon=get_icon(v)} end end,
+                               if_single_author = cosmo.c(not node.creator:match(",")){id=node.creator},
                                do_license = function() for i,v in split(node.license) do cosmo.yield{id=v} end end,
                                do_languages = function() for i,v in split(node.languages) do cosmo.yield{id=v} end end,
                                do_tags = function() for i,v in split(node.tags) do cosmo.yield{id=v} end end,
--- luaforge/lua/sputnik/node_defaults/@Project.lua
+++ luaforge/lua/sputnik/node_defaults/@Project.lua
@@ -23,6 +23,58 @@ NODE.html_content   = [=[
 
 <table class="project_info">
  <tr>
+  <td rowspan="7" style="background:#f6f6ff;border-right:none;">
+   <p style="font-size:120%"><b>$abstract</b></p>
+
+
+
+<table class="invisible" width="100%" valign="top">
+<tr>
+<td width="5%">
+<img src="http://media.freewisdom.org/tmp/web.png"/>
+</td>
+<td>
+   <p><a style="font-size:160%" href="$website">$website</a></p>
+</td>
+</tr>
+$if_releases[[
+<tr>
+<td width="5%">
+<img src="http://media.freewisdom.org/tmp/binary.png"/>
+</td>
+<td>
+$releases
+</td>
+</tr>
+]]
+$if_lists[[
+<tr>
+<td width="5%">
+<img src="http://media.freewisdom.org/tmp/mail.png"/>
+</td>
+<td>
+$lists
+</td>
+</tr>
+]]
+</table>
+
+  </td>
+</tr>
+<td width="20%" style="border-bottom:none;">
+  $do_authors[[<a title="$id" href="/people/$id"><img src="$icon"/></a>]]
+  $if_single_author[[<a style="vertical-align:super;" href="/people/$id">$id</a>]]
+
+</td>
+<tr><td style="border-bottom:none">$do_license[[$id license ]]</td></tr>
+<tr><td style="border-bottom:none">★★★</td></tr>
+<tr><td style="border-bottom:none">$do_languages[[$id]]&nbsp;</td></tr>
+<tr><td style="border-bottom:none">LR: n/a</td></tr>
+<tr><td style="font-size:90%">$do_tags[[$id]]&nbsp;</td></tr>
+</table>
+
+<!--table class="project_info">
+ <tr>
   <td rowspan="6" style="background:#f6f6ff;border-right:none;">
    <p style="font-size:120%"><b>$abstract</b></p>
 
@@ -36,19 +88,18 @@ NODE.html_content   = [=[
   $do_license[[$id license ]]
 </td>
 <tr><td style="border-bottom:none">★★★</td></tr>
-<tr><td style="border-bottom:none">$do_languages[[$id]]</td></tr>
+<tr><td style="border-bottom:none">$do_languages[[$id]]&nbsp;</td></tr>
 <tr><td style="border-bottom:none">LR: n/a</td></tr>
-<tr><td style="font-size:90%">$do_tags[[$id]]</td></tr>
-</table>
+<tr><td style="font-size:90%">$do_tags[[$id]]&nbsp;</td></tr>
+</table-->
 
-<h2>Archived Releases</h2>
+<!--<h2>Archived Releases</h2>
 
-$releases
 
 <h2>Mailing Lists</h2>
 
 $lists
-
+-->
 ]=]
 
 NODE.edit_ui        = [[
